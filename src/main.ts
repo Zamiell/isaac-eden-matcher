47d56998e6e3da8e716fd7697e79973d38c6677d
Zamiell <5511220+Zamiell@users.noreply.github.com>
2021-12-12 12:40:34 -0500

    initial commit

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,75 @@
-const MOD_NAME = "isaac-eden-matcher";
+import { getStartSeedString, onSetSeed } from "isaacscript-common";
+
+// Constants
+const MOD_NAME = "isaac-seed-checker";
+const VERBOSE = true;
+const ACTIVE_COLLECTIBLE_TO_LOOK_FOR = CollectibleType.COLLECTIBLE_EDENS_SOUL;
+// const PASSIVE_COLLECTIBLE_TO_LOOK_FOR = CollectibleType.COLLECTIBLE_TMTRAINER;
+// const CARD_TO_LOOK_FOR = Card.CARD_CHAOS;
+
+// Variables
+let restartOnNextFrame = false;
 
 export function main(): void {
-  // Instantiate a new mod object, which grants the ability to add callback functions that
-  // correspond to in-game events
   const mod = RegisterMod(MOD_NAME, 1);
 
-  // Set a callback function that corresponds to when a new run is started
-  mod.AddCallback(ModCallbacks.MC_POST_GAME_STARTED, postGameStarted);
+  mod.AddCallback(ModCallbacks.MC_POST_RENDER, postRender); // 2
+  mod.AddCallback(ModCallbacks.MC_POST_GAME_STARTED, postGameStarted); // 15
 
-  // Print an initialization message to the "log.txt" file
   Isaac.DebugString(`${MOD_NAME} initialized.`);
 }
 
-function postGameStarted() {
-  Isaac.DebugString("Callback triggered: MC_POST_GAME_STARTED");
+// ModCallbacks.MC_POST_RENDER (2)
+function postRender() {
+  if (restartOnNextFrame) {
+    restartOnNextFrame = false;
+    Isaac.ExecuteCommand("restart");
+  }
+}
+
+// ModCallbacks.MC_POST_GAME_STARTED (15)
+function postGameStarted(continued: boolean) {
+  const startSeedString = getStartSeedString();
+
+  if (VERBOSE) {
+    Isaac.DebugString(
+      `MC_POST_GAME_STARTED - ${startSeedString} - on set seed: ${onSetSeed()}`,
+    );
+  }
+
+  if (!validateRun(continued)) {
+    return;
+  }
+
+  const player = Isaac.GetPlayer();
+  if (player.HasCollectible(ACTIVE_COLLECTIBLE_TO_LOOK_FOR)) {
+    Isaac.DebugString(`GETTING HERE - ${startSeedString}`);
+    return;
+  }
+
+  restartOnNextFrame = true;
+}
+
+function validateRun(continued: boolean) {
+  const challenge = Isaac.GetChallenge();
+  const player = Isaac.GetPlayer();
+  const character = player.GetPlayerType();
+
+  if (continued) {
+    error(`The ${MOD_NAME} mod will not work when continuing a run.`);
+  }
+
+  if (challenge !== Challenge.CHALLENGE_NULL) {
+    error(`The ${MOD_NAME} mod will not work on challenges.`);
+  }
+
+  if (character !== PlayerType.PLAYER_EDEN) {
+    error(`The ${MOD_NAME} mod will not work on characters other than Eden.`);
+  }
+
+  if (onSetSeed()) {
+    error(`The ${MOD_NAME} mod will not work on set seeds.`);
+  }
+
+  return true;
 }
